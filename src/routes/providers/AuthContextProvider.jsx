1b8737b2b8e3823477f50628e384b4d6835dee6f
mhmahdi97 <mhmahdi97@gmail.com>
2023-05-04 17:53:34 +0600

    setup authContextProvider and react router

---
+++ src/routes/providers/AuthContextProvider.jsx
@@ -0,0 +1,61 @@
+// import all modules
+import React, { createContext } from 'react';
+import { getAuth } from "firebase/auth";
+import app from '../../utilities/firebase.config';
+
+
+// create and export react context
+export const AuthContext = createContext(null);
+
+// ger firebase auth
+const auth = getAuth(app);
+
+const AuthContextProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, loggedUser => {
+            console.log('logged in user inside auth state observer', loggedUser)
+            setUser(loggedUser);
+            setLoading(false);
+        })
+
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
+    
+     const authInfo = {
+        user,
+        loading,
+        createUser,
+        signIn,
+        logOut
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthContextProvider;
