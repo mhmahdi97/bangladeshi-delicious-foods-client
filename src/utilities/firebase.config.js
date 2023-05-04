cd011445f0d355c813de638bb7e0eda6f194a182
mhmahdi97 <mhmahdi97@gmail.com>
2023-05-04 17:30:49 +0600

    install configure firebase

---
+++ src/utilities/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDhthHetbafg3iVq7DZ3NvD1_C1jbDpSbQ",
+  authDomain: "bangladeshi-declicious-foods.firebaseapp.com",
+  projectId: "bangladeshi-declicious-foods",
+  storageBucket: "bangladeshi-declicious-foods.appspot.com",
+  messagingSenderId: "815221900015",
+  appId: "1:815221900015:web:00e87115e37e3c88c2911a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
