08b4b3da4065d6169c2c19ced0c314cc2ee645d6
mhmahdi97 <mhmahdi97@gmail.com>
2023-05-05 16:37:21 +0600

    fixed issues with banner section and footer

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -5,11 +5,11 @@ const Banner = () => {
         <>
         <div className="hero min-h-screen" style={{ backgroundImage: `url("https://i.ibb.co/1M8RNxx/banner-img-compressed.png")` }}>
             <div className="hero-overlay bg-opacity-40"></div>
-            <div className="hero-content text-center text-neutral-content">
+            <div className="hero-content text-center text-teal-50">
                 <div className="max-w-md">
                 <h1 className="mb-5 text-5xl font-bold">Welcome to Bangladeshi Delicious Foods</h1>
-                <p className="mb-5">Hungry? Wanna make some delicious foods? Discover the flavors of Bangladesh with our collection of delicious recipes and chef profiles! From traditional dishes to modern creations, our recipes is perfect for food lovers and home cooks. Learn from top Bangladeshi chefs and explore the unique spices and ingredients that make Bangladeshi cuisine so special. Get ready to take your taste buds on an unforgettable journey!</p>
-                <button className="btn btn-primary">Get Started</button>
+                <p className="mb-5 text-justify">Hungry? Wanna make some delicious foods? Discover the flavors of Bangladesh with our collection of delicious recipes and chef profiles! From traditional dishes to modern creations, our recipes is perfect for food lovers and home cooks. Learn from top Bangladeshi chefs and explore the unique spices and ingredients that make Bangladeshi cuisine so special. Get ready to take your taste buds on an unforgettable journey!</p>
+                <button className=" bg-teal-600  hover:bg-teal-700 px-4 py-2 rounded-md text-white font-semibold text-center">View Recipes</button>
                 </div>
             </div>
         </div>
