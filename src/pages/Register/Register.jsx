673372fc7a2af83c4d575233202f388787373db9
mhmahdi97 <mhmahdi97@gmail.com>
2023-05-05 14:01:39 +0600

    implemented register with email and password

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { AuthContext } from '../../routes/providers/AuthContextProvider';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+
+    const { createUser } = useContext(AuthContext);
+    const [error, setError] = useState('');
+
+    const handleRegister = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const photo = form.photo.value;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        setError('');
+
+        if(password.length < 6){
+            setError('Password must contain 6 characters or longer')
+            return
+        }
+
+        console.log(name, photo, email, password)
+        createUser(email, password)
+            .then(result => {
+                const createdUser = result.user;
+                console.log(createdUser);
+                <div className="toast toast-center toast-middle">
+                    <div className="alert alert-success">
+                        <div>
+                        <span>Rigistration Completed Successfully.</span>
+                        </div>
+                    </div>
+                </div>
+            })
+            .catch(error => {
+                console.log(error);
+                setError(error.message);
+            })
+    }
+
+
     return (
         <div className="hero min-h-screen bg-stone-100">
             <div className="hero-content flex-col">
                 <div className="text-center">
-                    <h1 className="text-4xl font-bold text-teal-800">Please Register to See Our Delicious Foods!</h1>
+                    <h1 className="text-3xl font-bold text-teal-800">Please Register to See Our Delicious Foods!</h1>
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-stone-100">
                     <form onSubmit={handleRegister} className="card-body">
@@ -19,7 +60,7 @@ const Register = () => {
                             <label className="label">
                                 <span className="label-text text-teal-800 text-lg font-bold">Photo URL</span>
                             </label>
-                            <input type="text" name='photo' placeholder="Enter Your Photo URL" className="input input-bordered" required />
+                            <input type="text" name='photo' placeholder="Enter Your Photo URL" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
@@ -43,16 +84,10 @@ const Register = () => {
                     </form>
     
                     <p className='mb-4 ml-8'>
-                        Already Have an Account? Login <Link to="/login" className="label-text-alt link link-hover text-blue-600">Here</Link>
+                        Already Have an Account? Login <Link to="/login" className="text-blue-600">Here</Link>
                     </p>
                     <div>
-                        <div className='flex justify-center mb-3'>
-                            <div>
-                                <button onClick={handleGoogleSignIn} className=" bg-stone-500 hover:bg-stone-600 px-4 py-2 mr-3 rounded-md text-white font-semibold text-center">Sign-In with Google <FaGoogle /> </button>
-                                <button onClick={handleGitHubSignIn} className=" bg-stone-500 hover:bg-stone-600 px-4 py-2 rounded-md text-white font-semibold text-center">Sign-In with Github  <FaGithub /> </button>
-                            </div>  
-                                    
-                        </div>
+                       
                     
                     </div>
                 </div>
